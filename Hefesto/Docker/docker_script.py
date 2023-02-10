778291c3cc03b4bea98654546ea8722b12dfe822
pabloalarconm <pabloalarconmoreno@gmail.com>
2023-02-10 11:47:24 +0100

    Docker corrections

--- Hefesto/Docker/docker_script.py
+++ Hefesto/Docker/docker_script.py
@@ -1,18 +1,20 @@
 from Hefesto.main import Hefesto
 from perseo.main import get_files
 from os import listdir, getcwd
-
-
+import pandas as pd
 
 mypath = getcwd()+ "/data"
 all_files = get_files(mypath,"csv")
+final_df = pd.DataFrame()
 
 for a in all_files:
 
     input_path = mypath + "/" + a
-    output_path = mypath + "/CDE_resulting_" + a
+    output_path = mypath + "/CDE.csv"
 
     # Test
     test = Hefesto(datainput = input_path)
     transform = test.transform_Fiab()
-    transform.to_csv (output_path, index = False, header=True)
+    final_df = pd.concat([final_df, transform])
+
+final_df.to_csv (output_path, index = False, header=True)
