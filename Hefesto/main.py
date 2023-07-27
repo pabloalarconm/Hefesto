e85c747189dddda072a41669a25989dde3f9031a
pabloalarconm <pabloalarconmoreno@gmail.com>
2023-07-27 12:33:08 +0200

    New Docker adaptation

--- Hefesto/main.py
+++ Hefesto/main.py
@@ -301,9 +301,10 @@ class Hefesto():
 
 # test = Hefesto(datainput = "../data/INPUT_DATA.csv")
 # transform = test.transformFiab()
-# transform.to_csv ("../data/OUTPUT_DATA.csv", index = False, header=True)
+# transform.to_csv ("../data/OUTPUT_DATA_new.csv", index = False, header=True)
 
-# Test 2
+# # Test 2:
+
 # with open("../data/CDEconfig.yaml") as file:
 #     configuration = yaml.load(file, Loader=yaml.FullLoader)
 
@@ -312,4 +313,4 @@ class Hefesto():
 # # label = test.get_label("output_type")
 # # url_from_label= test.get_uri("output_type_label","ncit")
 # # repl= test.replacement("output_type_label", "Date","DateXXX", duplicate=False)
-# transform.to_csv ("../data/OUTPUT_DATA2.csv", index = False, header=True)
+# transform.to_csv ("../data/OUTPUT_DATA2_new.csv", index = False, header=True)
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name="Hefesto",
-    version="0.4.5",
+    version="0.4.6",
     packages=["Hefesto"],
     author="Pablo Alarcón Moreno",
     author_email="pabloalarconmoreno@gmail.com",
